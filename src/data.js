a5da42f0941bcc81a91cade0f54daf0168da4bd0
Ayodele Agbowuro <ayodele.agbowuro@prunedge.com>
2024-02-01 10:58:06 +0100

    january

--- src/data.js
+++ src/data.js
@@ -1,31 +1,190 @@
 export const january = [
   {
-    name: "Ademola Olusegun",
-    birthday: "Feb 11th ",
-    department: "Media Department.",
+    id: 1,
+    name: "Ajayi Olumide",
+    birthday: "january 14th ",
+    department: "",
     image: "assets/INNOVA.jpg",
+    contact: "08181445383",
   },
   {
-    name: "Hester Hogan",
-    department: "Sanitation",
-    image: "https://www.course-api.com/images/people/person-2.jpeg",
+    id: 2,
+    name: "David Greatness",
+    birthday: "january 12th",
+    department: "",
+    image: "",
+    contact: "08089637328",
   },
   {
-    name: "Larry Little",
-    department: 36,
-    image: "https://www.course-api.com/images/people/person-3.jpeg",
+    id: 3,
+    name: "Oyeniran Biliki",
+    birthday: "January 16th",
+    department: "Good Women",
+    image: "",
+    contact: "08028286949",
   },
   {
-    name: "Sean Walsh",
-    department: 34,
-    image: "https://www.course-api.com/images/people/person-4.jpeg",
+    id: 4,
+    name: "Oluwaseun Adedeji",
+    birthday: "january 7th",
+    department: "",
+    image: "",
+    contact: "07031217114",
   },
   {
-    name: "Lola Gardner",
-    department: 29,
-    image: "https://www.course-api.com/images/people/person-5.jpeg",
+    id: 5,
+    name: "Adele Tolulope",
+    birthday: "January 16th ",
+    department: "Choir",
+    image: "",
+    contact: "08024662122",
+  },
+  {
+    id: 6,
+    name: "Usman Opeoluwa",
+    birthday: "January 24th",
+    department: "Children Church",
+    image: "",
+    contact: "08088929251",
+  },
+  {
+    id: 7,
+    name: "Oyeniran Oluwafemi",
+    birthday: "January 11th",
+    department: "Technical Department",
+    image: "",
+    contact: "08075054157",
+  },
+  {
+    id: 8,
+    name: "chika okechukwu",
+    birthday: "January 25th",
+    department: "",
+    image: "",
+    contact: "08164528851",
+  },
+  {
+    id: 9,
+    name: "Adeola Osagie",
+    birthday: "January 6th",
+    department: "",
+    image: "",
+    contact: "08022519576",
+  },
+  {
+    id: 10,
+    name: "Otele Esther",
+    birthday: "January 3rd",
+    department: "",
+    image: "",
+    contact: "08027627969, 0705130060",
   },
 ];
 export const february = [
-  { id: 5, name: "Lola Gardner", department: 29, image: "/assets/INNOVA.jpg" },
+  {
+    id: 1,
+    name: "",
+    birthday: "",
+    department: "",
+    image: "",
+    contact: "",
+  },
+
+  {
+    id: 2,
+    name: "",
+    birthday: "",
+    department: "",
+    image: "",
+  },
+];
+export const march = [
+  {
+    id: 2,
+    name: "",
+    birthday: "",
+    department: "",
+    image: "",
+  },
+];
+export const april = [
+  {
+    id: 2,
+    name: "",
+    birthday: "",
+    department: "",
+    image: "",
+  },
+];
+export const may = [
+  {
+    id: 2,
+    name: "",
+    birthday: "",
+    department: "",
+    image: "",
+  },
+];
+export const june = [
+  {
+    id: 2,
+    name: "",
+    birthday: "",
+    department: "",
+    image: "",
+  },
+];
+export const july = [
+  {
+    id: 2,
+    name: "",
+    birthday: "",
+    department: "",
+    image: "",
+  },
+];
+export const august = [
+  {
+    id: 2,
+    name: "",
+    birthday: "",
+    department: "",
+    image: "",
+  },
+];
+export const september = [
+  {
+    id: 2,
+    name: "",
+    birthday: "",
+    department: "",
+    image: "",
+  },
+];
+export const october = [
+  {
+    id: 2,
+    name: "",
+    birthday: "",
+    department: "",
+    image: "",
+  },
+];
+export const november = [
+  {
+    id: 2,
+    name: "",
+    birthday: "",
+    department: "",
+    image: "",
+  },
+];
+export const december = [
+  {
+    id: 2,
+    name: "",
+    birthday: "",
+    department: "",
+    image: "",
+  },
 ];
