2112efe95d9a9ffd9951b4f1dac7ce8de2615bff
Ayodele Agbowuro <ayodele.agbowuro@prunedge.com>
2024-02-02 12:07:54 +0100

    coming out good

--- src/Quote.js
+++ src/Quote.js
@@ -3,8 +3,10 @@ import React from "react";
 const Quote = () => {
   return (
     <div>
-      <span>Psalm 90:12</span> So teach us to number our days, that we may gain
-      a heart of wisdom.
+      <span className='bible'>Psalm 90:12</span>
+      <em className='quote'>
+        So teach us to number our days, that we may gain a heart of wisdom.
+      </em>
     </div>
   );
 };
