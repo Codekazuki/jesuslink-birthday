1b2f81416a36912e032ed5f0eae6da4b5ab99a12
Ayodele Agbowuro <ayodele.agbowuro@prunedge.com>
2024-03-16 22:42:00 +0100

    innoliz class

--- src/Quote.js
+++ src/Quote.js
@@ -5,7 +5,7 @@ const Quote = () => {
     <div>
       <span className='bible'>Psalm 90:12</span>
       <em className='quote'>
-        So teach us to number our days, that we may gain a heart of wisdom.
+        So teach us to number our days, that we may gain a heart of wisdom....
       </em>
     </div>
   );
