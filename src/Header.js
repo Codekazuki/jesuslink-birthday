58a7b08fa3f46946cabbfad61afabda3a1b69c1c
Ayodele Agbowuro <ayodele.agbowuro@prunedge.com>
2024-03-19 22:26:18 +0100

    innoliz class

--- src/Header.js
+++ src/Header.js
@@ -1,13 +1,20 @@
 import React from "react";
+import { useState } from "react";
 
 const Header = () => {
+  const [home, setHome] = useState("");
+  function handleHome() {
+    setHome("Hommie");
+  }
   return (
     <header className='header'>
       <div className='logo'>
         <img src='./assets/rccg-for-web.png' alt='Logo' />
       </div>
       <nav className='navigation'>
-        <a href='/'>Home</a>
+        <a href='/' onClick={handleHome}>
+          Home
+        </a>
         <a href='/about'>About</a>
         <a href='/services'>Departments</a>
       </nav>
