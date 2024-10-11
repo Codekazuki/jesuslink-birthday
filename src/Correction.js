88c81e41bcc61184816c99c7bfba2f0c02f16cc1
Ayodele Agbowuro <ayodele.agbowuro@prunedge.com>
2024-10-11 06:45:22 +0100

    sprint

--- src/Correction.js
+++ src/Correction.js
@@ -1,16 +1,16 @@
 import List from "./List";
 import "./correction.css";
-import { august } from "./data";
+import { october } from "./data";
 
 import React, { useState } from "react";
 
 const Correction = () => {
-  const [people, setPeople] = useState(august);
+  const [people, setPeople] = useState(october);
 
   return (
     <main>
       <section className='container'>
-        <h3>{people.length} birthdays this month of August</h3>
+        <h3>{people.length} birthdays this month of October</h3>
         <List people={people} />
       </section>
     </main>
